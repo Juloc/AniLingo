3d3f8ae771d195b1602d82c05e5f52414d0841fe
Juloc <juli.hiresch@gmail.com>
2026-09-24 13:18:47 +0200

    android: add Gradle foundation

---
+++ clients/android/core-model/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "de.juloc.anilingo.core.model"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
