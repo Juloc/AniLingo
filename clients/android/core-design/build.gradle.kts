da9fa3117fc97cbb8edc6f3f327742804a46f194
Juloc <juli.hiresch@gmail.com>
2026-09-24 13:20:05 +0200

    android: align foundation with AGP 9 built-in Kotlin

--- clients/android/core-design/build.gradle.kts
+++ clients/android/core-design/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "de.juloc.anilingo.core.design"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 26
@@ -17,9 +16,6 @@ android {
     }
 }
 
-kotlin {
-    jvmToolchain(17)
-}
 
 val generatedAssetsDir = layout.buildDirectory.dir("generated/playerDesign/assets")
 
