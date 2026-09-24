63f01b67a1249e318f71dcf85b74aeac5d8f0904
Juloc <juli.hiresch@gmail.com>
2026-09-24 13:22:07 +0200

    android: add shared client models and player foundation

---
+++ clients/android/core-design/src/main/kotlin/de/juloc/anilingo/core/design/PlayerDesignAssets.kt
@@ -0,0 +1,7 @@
+package de.juloc.anilingo.core.design
+
+object PlayerDesignAssets {
+    const val Tokens = "player-tokens.json"
+    const val Icons = "player-icons.json"
+    const val Actions = "player-actions.json"
+}
